cc411bc557c072bc0bea4826420b3814931fcb78
Adrien Faucon <adrien.faucon@gmail.com>
2019-07-28 21:18:24 +0200

    First version of exceptions.py

---
+++ ppm/exceptions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+ppm.exceptions
+==============
+This module contains the set of ppm exceptions.
+"""
+
+class BasePpmException(Exception):
+    """ 
+    The base class for all ppm exceptions.
+    It is not meant to be directly instantiated, unlike specific exceptions here below.
+    """
+
+class UndefinedPythonProjectsPath(BasePpmException):
+    """
+    The PYTON_PRJ environment variable is not declared.
+    """
+
+class Exception2(BasePpmException):
+    """
+    Write a description
+    """
