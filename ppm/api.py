4907f56431ad8aa7bdd80b1e4a53233061d15c68
Adrien Faucon <adrien.faucon@gmail.com>
2019-09-10 21:10:04 +0200

    parsing inside files looking for undeclared variables

--- ppm/api.py
+++ ppm/api.py
@@ -25,9 +25,18 @@ class Template():
         for root, dirs, files in os.walk(self.tempdir_object.name, topdown=False):
             if os.path.join(self.tempdir_object.name, '.git') not in root:
                 env = jinja2.Environment()
-                env2 = jinja2.Environment(loader=jinja2.FileSystemLoader(root))
+
                 for name in files:
+                    # Find undeclared variables in the file name
                     ast = env.parse(name)
+                    undeclared_variables_set = jinja2.meta.find_undeclared_variables(ast)
+                    self.unknown_parameters_set.update(undeclared_variables_set)
+
+                    # Find undeclared variables in the file content
+                    f = open(os.path.join(root, name), "r", encoding='utf-8')
+                    string = f.read()
+                    f.close()
+                    ast = env.parse(string)
                     undeclared_variables_set = jinja2.meta.find_undeclared_variables(ast)
                     self.unknown_parameters_set.update(undeclared_variables_set)
                     
